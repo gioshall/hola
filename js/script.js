7410c9b30005f50e4ae77e4b9bc77408ed75c5db
gio <giogioshall@yahoo.com.tw>
2015-12-02 00:51:05 +0800

    00

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $('.menu-left .header-menu').click(function(){
 	$('#container, .menu').toggleClass('show-menu');
+});
 
-})
+$('.list-pix').click(function(){
+	$('.list-pt').removeClass('selected');
+	$(this).addClass('selected');
+	$('.list-cont').removeClass('pt').addClass('pix');
+});
+$('.list-pt').click(function(){
+	$('.list-pix').removeClass('selected');
+	$(this).addClass('selected');
+	$('.list-cont').removeClass('pix').addClass('pt');
+});
